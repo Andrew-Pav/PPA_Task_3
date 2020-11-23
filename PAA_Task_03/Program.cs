5cad0b2fc156d2a1405b37355aa1353edc06395d
Andrew_Pav <igorjizn@mail.ru>
2020-11-23 13:42:26 +0300

    PAA_Task_03

---
+++ PAA_Task_03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PAA_Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfWrongPasswords = 0;
+            string secretLetter = "Тут ничего нет";
+            string password = "8688632";
+            string answerOfOwner;
+            string request = "Введите пароль: ";
+            while(3 != countOfWrongPasswords)
+            {
+                Console.Write(request);
+                answerOfOwner = Console.ReadLine();
+                if(answerOfOwner == password)
+                {
+                    Console.WriteLine(secretLetter);
+                    break;
+                }
+                else
+                {
+                    countOfWrongPasswords++;
+                    request = "Неверный пароль, попробуйте ещё раз: ";
+                }
+            }
+        }
+    }
+}
